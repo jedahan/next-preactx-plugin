06e48e3e96d014eaba75b8dcca28fc5a9d97e3aa
kennykimjr <kennykimjr@gmail.com>
2019-06-24 21:31:37 -0700

    Publish hotfix and increment bugfix version.

--- alias.js
+++ alias.js
@@ -2,7 +2,7 @@ const path = require('path')
 const moduleAliases = require('module-alias')
 
 module.exports = () => {
-  moduleAlias.addAlias('react', path.join(__dirname, '..', 'preact', 'compat'))
-  moduleAlias.addAlias('react-dom', path.join(__dirname, '..', 'preact', 'compat'))
-  moduleAlias.addAlias("react-ssr-prepass", path.join(__dirname, '..', 'preact-ssr-prepass'))
+  moduleAliases.addAlias('react', path.join(__dirname, '..', 'preact', 'compat'))
+  moduleAliases.addAlias('react-dom', path.join(__dirname, '..', 'preact', 'compat'))
+  moduleAliases.addAlias("react-ssr-prepass", path.join(__dirname, '..', 'preact-ssr-prepass'))
 }
